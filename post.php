a6ed2fbe9e8df326e27a6fb83f50d30b32bc5350
Lukasz <c3ypt1c@gmail.com>
2017-09-13 22:04:59 +0100

    Googdnight push

---
+++ post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html><?php include_once("config.php");?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title><?php echo $title?></title>
+</head>
+<body>
+
+<div class='title'><h1><?php echo $title;?></h1></div>
+
+<div class='content'>
+	<p>Make new post</p>
+	<form>
+		Password:Field
+		Title
+		Content:
+		Other Stuff....
+	</form>
+</div>
+
+<div class="footer">
+	<p> Attribution: <a href="http://www.vincenzovuono.com/">Vincenzo Vuono</a> for Gravity Font.<br>
+		Website licensed under <a href="">CC0 1.0 Universal</a><br>
+	</p>
+</div>
+
+</body>
+</html>
